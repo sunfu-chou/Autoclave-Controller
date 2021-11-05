db21a258cd54ef7ae647f02dc94dbf777a7a1538
sunfu-chou <sunfu.chou@gmail.com>
2021-11-05 12:24:43 +0800

    add lut and update  steady state

--- read.py
+++ read.py
@@ -27,10 +27,52 @@ class Controller:
         self.ki_temp = 0.0004
         self.kd_temp = 0.0
 
-        self.bp_press = []
-        self.table_press = []
-        self.bp_temp = []
-        self.table_temp = []
+        self.bp_press = [0.4782, 0.6701, 1.045, 1.6158, 2.3332, 3.4363, 3.8378, 4.3367, 6.4171, 9.4081]
+        self.table_press = [-1.7934, -2.2213, -2.6226, -3.2101, -3.78, -4.4052, -4.5385, -4.7455, -5.4985, -6.2973]
+        self.bp_temp = [
+            30.1872595638511,
+            31.6409528432436,
+            31.9704263189301,
+            42.179105008502,
+            63.6552080230948,
+            63.6898877078096,
+            63.7978469738978,
+            64.4951212851203,
+            85.4914717116415,
+            111.485040539424,
+            113.59430155581,
+            113.695598993841,
+            129.212866342344,
+            139.138331793732,
+            140.406275335562,
+            144.483082776884,
+            145.130889411771,
+            151.665873380536,
+            165.504586356226,
+            165.547908995439,
+        ]
+        self.table_temp = [
+            -2.40592041889992,
+            -2.40193714475503,
+            -1.96606164384795,
+            -1.88280410083362,
+            -1.83294240935630,
+            -1.77167222675508,
+            -1.70512504336492,
+            -1.53387209202276,
+            -1.26307193253703,
+            -1.25128960532926,
+            -1.22594106279314,
+            -0.721695991192625,
+            -0.328835604819290,
+            -0.313099765124867,
+            -0.306065933410559,
+            -0.299478277011540,
+            -0.0826493895778796,
+            0.252424838633181,
+            0.264867254792093,
+            0.335251202542455,
+        ]
 
         # SS
         self.x1_past = 0
@@ -40,13 +82,13 @@ class Controller:
         self.x5_past = 0
         self.x6_past = 0
 
-        self.u_steady_state = 0.2281
-        self.x1_steady_state = 3.223e06
-        self.x2_steady_state = -1.442e05
-        self.x3_steady_state = 0.2028
-        self.x4_steady_state = -0.2615
-        self.x5_steady_state = 0.1448
-        self.x6_steady_state = 0.4438
+        self.u_steady_state = 0.1608
+        self.x1_steady_state = 2.72e06
+        self.x2_steady_state = -1.017e05
+        self.x3_steady_state = 0.143
+        self.x4_steady_state = -0.1844
+        self.x5_steady_state = 0.1021
+        self.x6_steady_state = 0.3129
         self.K1 = 6.850961097434739e-04
         self.K2 = -3.013769863835386e-04
         self.K3 = -2.284484679905255e-06
@@ -87,7 +129,7 @@ class Controller:
         return self.press_fb
 
     def setTempLUT(self, temp):
-        idx = bisect.bisect_left(self.bp_press, temp)
+        idx = bisect.bisect_left(self.bp_temp, temp)
         self.temp_fb = self.table_temp[idx - 1] + (self.table_temp[id] - self.table_temp[id - 1]) * (
             temp - self.bp_temp[id - 1]
         ) / (self.bp_temp[id] - self.bp_temp[id - 1])
